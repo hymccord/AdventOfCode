2a035c9351d3b401c8fcc486ef52a9973f45658a
Hank McCord <Henry.McCord@ewudn.robins.af.mil>
2019-12-09 15:37:06 -0500

    Add 2019 Day 4

--- Solutions/Year2019/Day04/Solution.cs
+++ Solutions/Year2019/Day04/Solution.cs
@@ -1,17 +1,83 @@
+#nullable enable
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day04 : ASolution {
-        
+        int low;
+        int high;
+
         public Day04() : base(4, 2019, "") {
-            
+            int[] input = Input.Split('-').Select(int.Parse).ToArray();
+            low = input[0];
+            high = input[1];
         }
 
         protected override string SolvePartOne() {
-            return null; 
+            int valid = 0;
+            for (int i = low; i <= high; i++)
+            {
+                bool doubled = false;
+                bool increasing = true;
+
+                int previousDigit = i % 10;
+                int num = i / 10;
+                while (num > 0 && increasing)
+                {
+                    int curDigit = num % 10;
+
+                    if (curDigit == previousDigit)
+                        doubled = true;
+
+                    increasing &= curDigit <= previousDigit;
+
+                    previousDigit = curDigit;
+                    num /= 10;
+                }
+
+                if (doubled && increasing)
+                    valid++;
+            }
+
+            return valid.ToString();
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+            int valid = 0;
+            for (int i = low; i <= high; i++)
+            {
+                bool doubled = false;
+                bool increasing = true;
+
+                int previousDigit = i % 10;
+                int num = i / 10;
+                while (num > 0 && increasing)
+                {
+                    int curDigit = num % 10;
+
+                    int extraDigits = 0;
+                    while (curDigit == previousDigit)
+                    {
+                        extraDigits++;
+                        previousDigit = curDigit;
+                        num /= 10;
+                        curDigit = num % 10;
+                    }
+
+                    if (extraDigits == 1)
+                        doubled = true;
+
+                    increasing &= curDigit <= previousDigit;
+
+                    previousDigit = curDigit;
+                    num /= 10;
+                }
+
+                if (doubled && increasing)
+                    valid++;
+            }
+
+            return valid.ToString();
         }
     }
 }
