b9d8577be0cea26091369caebcce10f83d403c61
Hank McCord <Henry.McCord@ewudn.robins.af.mil>
2019-12-09 15:36:47 -0500

    Add 2019 Day 2

--- Solutions/Year2019/Day02/Solution.cs
+++ Solutions/Year2019/Day02/Solution.cs
@@ -1,17 +1,46 @@
+using System;
+using System.Linq;
+
 namespace AdventOfCode.Solutions.Year2019 {
 
     class Day02 : ASolution {
         
+        private int[] test = new int[] { 1,1,1,4,99,5,6,0,99 };
+        private int[] intInputs;
+
         public Day02() : base(2, 2019, "") {
-            
+            intInputs = Input.Split(',', System.StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();    
+            //intInputs = test;
         }
 
         protected override string SolvePartOne() {
-            return null; 
+            IntCode cpu = IntCode.Create(Input.Trim('\n'));
+            cpu.ByteCode[1] = 12;
+            cpu.ByteCode[2] = 2;
+            cpu.Run();
+
+            return cpu.ByteCode[0].ToString();
         }
 
         protected override string SolvePartTwo() {
-            return null; 
+            IntCode cpu = IntCode.Create(Input.Trim('\n'));
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    cpu.Reset();
+                    cpu.ByteCode[1] = i;
+                    cpu.ByteCode[2] = j;
+                    cpu.Run();
+
+                    if (cpu.ByteCode[0] == 19690720)
+                        return $"100 * {i} + {j} = {100 * i + j}";
+
+                }
+            }            
+
+            return null;
         }
     }
 }
---
+++ Solutions/Year2019/IntCode.cs
@@ -0,0 +1,523 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Solutions.Year2019
+{
+    internal class IntCode
+    {
+        public Func<int, int> GetInput { get; set; } = delegate { return 0; };
+
+        public event EventHandler Halt;
+        public event EventHandler<int> Output;
+
+        ReadOnlyMemory<int> program;
+        readonly IntCodeInstruction[] _opCodeHandlers;
+
+        internal State state;
+        internal bool askInput = false;
+
+        public int[] ByteCode => state.memory;
+
+        public int InstructionPointer { get; internal set; }
+
+        internal struct State
+        {
+            public int[] memory;
+            public int inputRequest;
+        }
+
+        static IntCode()
+        {
+            _ = InstructionOpCounts.OpCount;
+        }
+
+        IntCode(string input)
+        {
+            _opCodeHandlers = OpCodeHandlersTable.Handlers;
+            program = input.Split(',').Select(int.Parse).ToArray();
+            state.memory = new int[program.Length];
+            program.CopyTo(state.memory);
+        }
+
+        public static IntCode Create(string input)
+        {
+            return new IntCode(input);
+        }
+
+        public void Reset()
+        {
+            InstructionPointer = 0;
+            state.inputRequest = 0;
+            program.CopyTo(state.memory);
+        }
+
+        public void Run()
+        {
+            int opCode;
+            while (true)
+            {
+                opCode = Read();
+                if (opCode % 100 == 99)
+                {
+                    Halt?.Invoke(this, EventArgs.Empty);
+                    break;
+                }
+
+                Decode(_opCodeHandlers[opCode % 100], opCode / 100);
+            }
+        }
+
+        private void Decode(IntCodeInstruction handler, int parameterMode)
+        {
+            handler.Execute(this, parameterMode);
+        }
+
+        private int Read()
+        {
+            return state.memory[InstructionPointer++];
+        }
+
+        internal void RaiseOutput(int s) => Output?.Invoke(this, s);
+    }
+    abstract class IntCodeInstruction
+    {
+        public IntCodeInstruction(OpCode code)
+        {
+            Code = code;
+        }
+        public OpCode Code { get; internal set; }
+
+        public int OpCount => InstructionOpCounts.OpCount[(int)Code];
+
+        public abstract void Execute(IntCode decoder, int parameterMode);
+
+        protected int GetParameter(int mode, int pointer, int[] byteCode)
+        {
+            return mode == 0 ? byteCode[byteCode[pointer]] : byteCode[pointer];
+        }
+
+        protected void SetParameter(int mode, int pointer, int[] byteCode, int value)
+        {
+            if (mode == 0)
+            {
+                byteCode[byteCode[pointer]] = value;
+            }
+            else
+            {
+                byteCode[pointer] = value;
+            }
+        }
+    }
+
+    sealed class halt : IntCodeInstruction
+    {
+        public halt() : base(OpCode.halt)
+        {
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode) { }
+    }
+
+    sealed class invalid : IntCodeInstruction
+    {
+        public static readonly invalid Instance = new invalid();
+        invalid() : base(OpCode.invalid) { }
+        public override void Execute(IntCode decoder, int parameterMode) => throw new Exception();
+    }
+
+
+    class addm : IntCodeInstruction
+    {
+        public addm() : base(OpCode.addr)
+        {
+
+        }
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+            int p2 = parameterMode / 10 % 2;
+            int p3 = parameterMode / 100 % 2;
+            int m1 = GetParameter(p1, decoder.InstructionPointer++, state.memory);
+            int m2 = GetParameter(p2, decoder.InstructionPointer++, state.memory);
+
+            SetParameter(p3, decoder.InstructionPointer++, state.memory, m1 + m2);
+        }
+    }
+
+    class mulm : IntCodeInstruction
+    {
+        public mulm() : base(OpCode.mulr)
+        {
+
+        }
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+            int p2 = parameterMode / 10 % 2;
+            int p3 = parameterMode / 100 % 2;
+            int m1 = GetParameter(p1, decoder.InstructionPointer++, state.memory);
+            int m2 = GetParameter(p2, decoder.InstructionPointer++, state.memory);
+
+            SetParameter(p3, decoder.InstructionPointer++, state.memory, m1 * m2);
+        }
+    }
+
+    class input : IntCodeInstruction
+    {
+        public input() : base(OpCode.input)
+        {
+
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+
+            if (decoder.askInput) Console.Write("Input: ");
+            SetParameter(p1, decoder.InstructionPointer++, state.memory, decoder.GetInput(state.inputRequest++));
+
+            //state.instructionsRun.Add("Store")
+        }
+    }
+
+    class output : IntCodeInstruction
+    {
+        public output() : base(OpCode.output)
+        {
+
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+
+            decoder.RaiseOutput(GetParameter(parameterMode % 2, decoder.InstructionPointer++, state.memory));
+        }
+    }
+
+    class jmptrue : IntCodeInstruction
+    {
+        public jmptrue() : base(OpCode.jmptrue)
+        {
+
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+            int p2 = parameterMode / 10 % 2;
+            int m1 = GetParameter(p1, decoder.InstructionPointer++, state.memory);
+            int m2 = GetParameter(p2, decoder.InstructionPointer++, state.memory);
+
+            if (m1 != 0)
+                decoder.InstructionPointer = m2;
+        }
+    }
+
+    class jmpfalse : IntCodeInstruction
+    {
+        public jmpfalse() : base(OpCode.jmpfalse)
+        {
+
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+            int p2 = parameterMode / 10 % 2;
+            int m1 = GetParameter(p1, decoder.InstructionPointer++, state.memory);
+            int m2 = GetParameter(p2, decoder.InstructionPointer++, state.memory);
+
+            if (m1 == 0)
+                decoder.InstructionPointer = m2;
+        }
+    }
+
+    class slt : IntCodeInstruction
+    {
+        public slt() : base(OpCode.slt)
+        {
+
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+            int p2 = parameterMode / 10 % 2;
+            int p3 = parameterMode / 100 % 2;
+            int m1 = GetParameter(p1, decoder.InstructionPointer++, state.memory);
+            int m2 = GetParameter(p2, decoder.InstructionPointer++, state.memory);
+
+            SetParameter(p3, decoder.InstructionPointer++, state.memory, m1 < m2 ? 1 : 0);
+        }
+    }
+
+    class eq : IntCodeInstruction
+    {
+        public eq() : base(OpCode.eq)
+        {
+
+        }
+
+        public override void Execute(IntCode decoder, int parameterMode)
+        {
+            ref var state = ref decoder.state;
+            int p1 = parameterMode % 2;
+            int p2 = parameterMode / 10 % 2;
+            int p3 = parameterMode / 100 % 2;
+            int m1 = GetParameter(p1, decoder.InstructionPointer++, state.memory);
+            int m2 = GetParameter(p2, decoder.InstructionPointer++, state.memory);
+
+            SetParameter(p3, decoder.InstructionPointer++, state.memory, m1 == m2 ? 1 : 0);
+        }
+    }
+
+    enum OpCode
+    {
+        invalid,
+        addr,
+        mulr,
+        input,
+        output,
+        jmptrue,
+        jmpfalse,
+        slt,
+        eq,
+        halt = 99,
+    }
+
+    static class DecoderConstants
+    {
+        public const int NumberOfCodeValues = 100;
+    }
+
+    static class InstructionOpCounts
+    {
+        internal static byte[] OpCount = new byte[DecoderConstants.NumberOfCodeValues]
+        {
+            0,//invalid
+            3,//addr
+            3,//mulr
+            1,//input
+            1,//output
+            2,//jmptrue
+            2,//jmpfalse
+            3,//slt
+            3,//eq
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//invalid
+            0,//halt
+        };
+    }
+
+    static class OpCodeHandlersTable
+    {
+        internal static readonly IntCodeInstruction[] Handlers;
+
+        static OpCodeHandlersTable()
+        {
+            var invalid = Year2019.invalid.Instance;
+
+            Handlers = new IntCodeInstruction[100]
+            {
+                invalid,
+                new addm(),
+                new mulm(),
+                new input(),
+                new output(),
+                new jmptrue(),
+                new jmpfalse(),
+                new slt(),
+                new eq(),
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                invalid,
+                new halt(),
+            };
+        }
+    }
+}
