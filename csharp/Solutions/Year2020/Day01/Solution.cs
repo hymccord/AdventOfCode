9dbb5fbfdeca0a9de0783d0507f835e936184a22
Hank McCord <hymccord@gmail.com>
2020-12-02 10:37:59 -0500

    Add 2020 Day 01

---
+++ csharp/Solutions/Year2020/Day01/Solution.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Solutions;
+
+class Solution : ASolution
+{
+    private HashSet<int> _numSet;
+    private List<int> _numList;
+
+    public Solution() :
+        base(1, 2020, "Report Repair")
+    {
+        _numSet = Input.SplitByNewline().Select(int.Parse).ToHashSet();
+        _numList = _numSet.ToList();
+    }
+
+    protected override string SolvePartOne()
+    {
+        foreach (var num in _numSet)
+        {
+            if (_numSet.Contains(2020 - num))
+                return (num * (2020 - num)).ToString();
+        }
+
+        throw new KeyNotFoundException();
+    }
+
+    protected override string SolvePartTwo()
+    {
+        Dictionary<int, (int, int)> pairs = new(); 
+        for (int i = 0; i < _numList.Count - 1; i++)
+        {
+            for (int j = i + 1; j < _numList.Count; j++)
+            {
+                int one = _numList[i];
+                int two = _numList[j];
+                pairs[one + two] = (one, two);
+            }
+        }
+
+        foreach (var num in _numSet)
+        {
+            if (pairs.ContainsKey(2020 - num))
+            {
+                (int x, int y) = pairs[2020 - num];
+                return (num * x * y).ToString();
+            }
+        }
+        throw new KeyNotFoundException();
+    }
+}
