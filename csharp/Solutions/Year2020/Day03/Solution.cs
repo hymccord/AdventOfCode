4afd4dde7ed77bc90e17f144addbfe8795ec7e3a
Hank McCord <hymccord@gmail.com>
2020-12-03 12:40:27 -0500

    Add 2020 Day 03

---
+++ csharp/Solutions/Year2020/Day03/Solution.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Year2020
+{
+    class Day03 : ASolution
+    {
+        private char[][] trees;
+        int width;
+        int height;
+
+        private string test = @"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+        public Day03() : base(3, 2020, "Toboggan Trajectory")
+        {
+            trees = Input.SplitByNewline().Select(s => s.ToCharArray()).ToArray();
+            //trees = test.SplitByNewline().Select(s => s.ToCharArray()).ToArray();
+            width = trees[0].Length;
+            height = trees.Length;
+
+            WriteConsole(height, width, 0, 0, (row, col) => 
+            {
+                return (ConsoleColor.White, trees[row][col]);
+            });
+        }
+
+        protected override string SolvePartOne()
+        {
+            return Toboggan(new Point(3, 1)).ToString();
+        }
+
+        protected override string SolvePartTwo()
+        {
+            WriteConsole(height, width, 0, 0, (row, col) =>
+            {
+                return (ConsoleColor.White, trees[row][col]);
+            });
+
+            return (Toboggan(new Point(1, 1))
+                * Toboggan(new Point(3, 1))
+                * Toboggan(new Point(5, 1))
+                * Toboggan(new Point(7, 1))
+                * Toboggan(new Point(1, 2), true)).ToString();
+        }
+
+        private long Toboggan(Point slope, bool print = false)
+        {
+            Point p = new Point(0, 0);
+            long count = 0;
+            while (true)
+            {
+                p += slope;
+
+                if (p.Y >= height)
+                    break;
+
+                if (trees[p.Y][p.X % width] == '#')
+                {
+                    if (print)
+                        WriteConsole(p.Y, p.X % width, 0, 0, 'X', ConsoleColor.Red);
+                    count++;
+                }
+                else if (print)
+                {
+                    WriteConsole(p.Y, p.X % width, 0, 0, 'O', ConsoleColor.Green);
+                }
+            }
+
+            return count;
+        }
+    }
+}
