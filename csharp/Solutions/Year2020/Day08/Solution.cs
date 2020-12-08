a33f496967aac292c1b1b23993803831b3382e45
Hank McCord <hymccord@gmail.com>
2020-12-08 15:44:53 -0500

    Add 2020 Day 08

---
+++ csharp/Solutions/Year2020/Day08/Solution.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions.Year2020.Day08
+{
+    class Day08 : ASolution
+    {
+        public Day08() :
+            base(08, 2020, "Handheld Halting")
+        { }
+
+        protected override object SolvePartOne()
+        {
+            var hgc = HandheldGameConsole.Create(Input);
+            hgc.Run();
+            return hgc.Acc;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            int i = 0;
+            while (true)
+            {
+                var hgc = HandheldGameConsole.Create(Input);
+                hgc.FlipInstructin(i);
+                if (hgc.Run())
+                {
+                    i = hgc.Acc;
+                    break;
+                }
+                i++;
+            }
+
+            return i;
+        }
+
+        private class HandheldGameConsole
+        {
+            private (string instr, int arg)[] _program;
+            private int _rip = 0;
+            private int _acc = 0;
+            private HashSet<int> _alreadyRun = new();
+
+            public int Rip => _rip;
+            public int Acc => _acc;
+
+            HandheldGameConsole(string input)
+            {
+                _program = input.SplitByNewline().Select(s =>
+                {
+                    var instr = s.Split(' ');
+                    return (instr[0], int.Parse(instr[1]));
+                }
+                ).ToArray();
+            }
+
+            public static HandheldGameConsole Create(string input)
+            {
+                return new HandheldGameConsole(input);
+            }
+
+            public void FlipInstructin(int i)
+            {
+                var (instr, arg) = _program[i];
+                instr = instr switch
+                {
+                    "jmp" => "nop",
+                    "nop" => "jmp",
+                    _ => instr,
+                };
+                _program[i] = (instr, arg);
+            }
+
+            public bool Run()
+            {
+                while (!_alreadyRun.Contains(_rip) && _rip < _program.Length)
+                {
+                    _alreadyRun.Add(_rip);
+                    var (instr, arg) = _program[_rip];
+
+                    switch (instr)
+                    {
+                        case "jmp":
+                            _rip += arg;
+                            break;
+                        case "acc":
+                            _acc += arg;
+                            _rip++;
+                            break;
+                        case "nop":
+                        default:
+                            _rip++;
+                            break;
+                    }
+                }
+
+                return _rip >= _program.Length;
+            }
+        }
+    }
+}
