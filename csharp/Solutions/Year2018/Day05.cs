9dd7a9f47431ee7704fcf6726e27ffb4047c5a8e
Hank McCord <hymccord@gmail.com>
2021-11-23 11:48:27 -0500

    Add and convert old 2018 Day05

---
+++ csharp/Solutions/Year2018/Day05.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Solutions.Year2018
+{
+    class Day05 : ASolution
+    {
+        const string test = "dabAcCaCBAcCcaDA";
+
+        public Day05() : base(05, 2018, "")
+        {
+
+        }
+
+        protected override object SolvePartOne()
+        {
+            string input = Input.SplitByNewline().First();
+            //string input = SplitInput(test).First();
+
+            input = React(input);
+
+            return input.Length;
+        }
+
+        protected override object SolvePartTwo()
+        {
+            string originalInput = Input.SplitByNewline().First();
+
+            int minL = int.MaxValue;
+            for (int i = 0; i < 26; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (char c in originalInput)
+                {
+                    if (!(c == 'a' + i || c == 'A' + i))
+                    {
+                        sb.Append(c);
+                    }
+                }
+                string s = React(sb.ToString());
+
+                if (s.Length < minL)
+                    minL = s.Length;
+
+            }
+
+            return minL;
+        }
+
+        private string React(string s)
+        {
+            for (int i = 0; i < s.Length - 1;)
+            {
+                if ((s[i] ^ s[i + 1]) == 32)
+                {
+                    s = s.Remove(i, 2);
+                    i--;
+
+                    if (i < 0)
+                        i = 0;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return s;
+        }
+    }
+}
