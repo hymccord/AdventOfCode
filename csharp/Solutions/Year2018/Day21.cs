905c825521110873180a1894181dfae34e4cd8a5
Hank McCord <hymccord@gmail.com>
2021-11-24 14:17:59 -0500

    Add and convert old 2018 Day21

---
+++ csharp/Solutions/Year2018/Day21.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AdventofCode.ELF_Lang;
+
+namespace AdventOfCode.Solutions.Year2018
+{
+    internal class Day21 : ASolution
+    {
+        public Day21() : base(21, 2018)
+        {
+
+        }
+
+        protected override object SolvePartOne()
+        {
+            var input = Input.SplitByNewline();
+            CPU.InstructionRegister = int.Parse(input[0].Substring(4, 1));
+            CPU.Program = input.Skip(1).Select(Instruction.Parse).ToArray();
+
+            CPU.RegRead += (s, e) =>
+            {
+                //PART 1;
+                if (CPU.Register[CPU.InstructionRegister] == 28)
+                {
+                    //Console.WriteLine(CPU.Register[3]);
+                    CPU.Register[0] = CPU.Register[3];
+                    throw new Exception();
+                }
+            };
+
+            try
+            {
+                CPU.Execute();
+            }
+            catch (Exception)
+            {}
+
+            return CPU.Register[0];
+        }
+
+        protected override object SolvePartTwo()
+        {
+            HashSet<int> repeats = new HashSet<int>();
+            int a = 0;
+            int b = 0;
+
+            while (true)
+            {
+                a = b | 65536;
+                b = 0x16F8CB;
+                while (true)
+                {
+                    b += a & 0xff;
+                    b &= 0xFF_FFFF;
+                    b *= 0x1016B;
+                    b &= 0xFF_FFFF;
+
+                    if (a >= 256)
+                    {
+                        a /= 256;
+                        continue;
+                    }
+                    else
+                    {
+                        if (!repeats.Contains(b))
+                        {
+                            repeats.Add(b);
+                        }
+                        else
+                        {
+                            //Console.WriteLine(repeats.Last());
+                            return repeats.Last();
+                        }
+                        break;
+                    }
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
