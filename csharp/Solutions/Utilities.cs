493f8dce17cc801a35d54f52a428d69ad62a4f8a
Hank McCord <hymccord@gmail.com>
2021-12-04 02:35:29 -0500

    Add 2D array transpose extension

--- csharp/Solutions/Utilities.cs
+++ csharp/Solutions/Utilities.cs
@@ -166,6 +166,37 @@ namespace AdventOfCode.Solutions
             }
         }
 
+        public static T[,] Transpose<T>(this T[,] arr)
+        {
+            int rowCount = arr.GetLength(0);
+            int columnCount = arr.GetLength(1);
+            T[,] transposed = new T[columnCount, rowCount];
+            if (rowCount == columnCount)
+            {
+                transposed = (T[,])arr.Clone();
+                for (int i = 1; i < rowCount; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        T temp = transposed[i, j];
+                        transposed[i, j] = transposed[j, i];
+                        transposed[j, i] = temp;
+                    }
+                }
+            }
+            else
+            {
+                for (int column = 0; column < columnCount; column++)
+                {
+                    for (int row = 0; row < rowCount; row++)
+                    {
+                        transposed[column, row] = arr[row, column];
+                    }
+                }
+            }
+            return transposed;
+        }
+
         public static IEnumerable<T> SliceRow<T>(this T[,] arr, int row)
         {
             for (int i = 0; i < arr.GetLength(1); i++)
