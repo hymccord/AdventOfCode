54816f3bf9285e8a075440cdad61b0975daa2fec
Hank McCord <hymccord@gmail.com>
2021-12-06 01:58:43 -0500

    2021 Day 06

---
+++ csharp/Solutions/Year2021/Day06.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Solutions.Year2021
+{
+    internal class Day06 : ASolution
+    {
+        public Day06() : base(06, 2021, "Lanternfish", false)
+        { }
+
+        protected override object SolvePartOne()
+        {
+            return GetDaFish(80);
+        }
+
+        protected override object SolvePartTwo()
+        {
+            return GetDaFish(256);
+        }
+
+        private long GetDaFish(int days)
+        {
+            long[] fish = new long[9];
+            foreach (var item in Input.ToIntArray(','))
+            {
+                fish[item] += 1;
+            }
+
+            int day = 0;
+            while (day < days)
+            {
+                long spawn = fish[0];
+                for (int i = 1; i <= 8; i++)
+                {
+                    fish[i - 1] = fish[i];
+                }
+                fish[6] += spawn;
+                fish[8] = spawn;
+                day++;
+            }
+
+            return fish.Sum();
+        }
+
+        protected override string LoadDebugInput()
+        {
+            return @"3,4,3,1,2";
+        }
+    }
+}
