12d66fa62194c825b37bb7810c6c4916bb13762a
Hank McCord <hymccord@gmail.com>
2022-12-01 00:34:06 -0500

    Add 2022 Day 01

--- csharp/Solutions/Year2022/Day01.cs
+++ csharp/Solutions/Year2022/Day01.cs
@@ -2,23 +2,40 @@
 
 internal class Day01 : ASolution
 {
-    public Day01() : base(1, 2022, "", false)
-    {
+    private readonly Lazy<int[]> _nums;
 
+    public Day01() : base(1, 2022, "Calorie Counting", false)
+    {
+        _nums = new(Input.SplitByBlankLine().Select(s => s.ToIntArray().Sum()).ToArray());
     }
 
     protected override object SolvePartOne()
     {
-        return null;
+        return _nums.Value.Max();
     }
 
     protected override object SolvePartTwo()
     {
-        return null;
+        return _nums.Value.OrderDescending().Take(3).Sum();
     }
 
     protected override string LoadDebugInput()
     {
-        return @"";
+        return """
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+            """;
     }
 }
