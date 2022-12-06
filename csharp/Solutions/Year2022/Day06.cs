6d0871667e0000e62a9c5cf6cd18becf185e2420
Hank McCord <hymccord@gmail.com>
2022-12-06 00:53:55 -0500

    Add 2022 Day 06

---
+++ csharp/Solutions/Year2022/Day06.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Solutions.Year2022;
+
+internal class Day06 : ASolution
+{
+    public Day06() : base(06, 2022, "", false)
+    { }
+
+    protected override object SolvePartOne()
+    {
+        return Tune(4);
+    }
+
+    protected override object SolvePartTwo()
+    {
+        return Tune(14);
+    }
+
+    private int Tune(int numCharacters)
+    {
+        for (int i = 0; i < Input.Length; i++)
+        {
+            if (new HashSet<char>(Input.Substring(i, numCharacters).ToCharArray()).Count == numCharacters)
+            {
+                return i + numCharacters;
+            }
+        }
+
+        return -1;
+    }
+
+    protected override string LoadDebugInput()
+    {
+        return """
+        mjqjpqmgbljsphdztnvjfqwrcgsmlb
+        """;
+    }
+}
