aad3f0fcd39dc3def72d96924f793f28722504dd
Hank McCord <hymccord@gmail.com>
2023-12-01 13:03:24 -0500

    Add 2023 Day 01

---
+++ csharp/Solutions/Year2023/Day01.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace AdventOfCode.Solutions.Year2023;
+
+internal class Day01 : ASolution
+{
+    public Day01() : base(01, 2023, "Trebuchet?!", false)
+    { }
+
+    protected override object SolvePartOne()
+    {
+        return InputByNewLine.Select(x => x.Where(char.IsAsciiDigit))
+            .Sum(ic => int.Parse($"{ic.First()}{ic.Last()}"));
+    }
+
+    protected override object SolvePartTwo()
+    {
+        var replacement = new Dictionary<string, int>
+        {
+            { "one", 1},
+            { "two", 2},
+            { "three", 3},
+            { "four", 4},
+            { "five", 5},
+            { "six", 6},
+            { "seven", 7},
+            { "eight", 8},
+            { "nine", 9},
+            { "1", 1},
+            { "2", 2},
+            { "3", 3},
+            { "4", 4},
+            { "5", 5},
+            { "6", 6},
+            { "7", 7},
+            { "8", 8},
+            { "9", 9},
+        };
+
+        return InputByNewLine
+            .Select(line =>
+            {
+                // Overlap match groups by using lookahead (?=).
+                return Regex.Matches(line, "(?=(one|two|three|four|five|six|seven|eight|nine|1|2|3|4|5|6|7|8|9))");
+            })
+            .Sum(mc =>
+            {
+                // Use first and last matches.
+                const int LookaheadGroup = 1;
+                Index FirstMatch = 0;
+                Index LastMatch = ^1;
+                return replacement[mc[FirstMatch].Groups[LookaheadGroup].Value] * 10
+                    + replacement[mc[LastMatch].Groups[LookaheadGroup].Value];
+            });
+    }
+
+    protected override string LoadDebugInput()
+    {
+        return """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        """;
+    }
+}
