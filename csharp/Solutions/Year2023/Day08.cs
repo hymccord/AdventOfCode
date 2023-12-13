b00f17f474e893b5b26c21c4c30cfa5e1018fc09
Hank McCord <hymccord@gmail.com>
2023-12-12 20:25:51 -0500

    Floyd for Day 08 is apparently overthinking it. That's AoC for you.

--- csharp/Solutions/Year2023/Day08.cs
+++ csharp/Solutions/Year2023/Day08.cs
@@ -1,9 +1,4 @@
-﻿
-
-
-using MoreLinq;
-
-namespace AdventOfCode.Solutions.Year2023;
+﻿namespace AdventOfCode.Solutions.Year2023;
 
 internal class Day08 : ASolution
 {
@@ -34,14 +29,21 @@ internal class Day08 : ASolution
     protected override object SolvePartTwo()
     {
         string[] current = _map.Keys.Where(s => s.EndsWith("A")).ToArray();
-        (int lam, int mu)[] res = new (int lam, int mu)[current.Length];
+        int[] cycles = new int[current.Length];
 
-        for (int i = 0; i < res.Length; i++)
+        for (int i = 0; i < current.Length; i++)
         {
-            res[i] = Floyd(Stepper, (0, current[i]));
+            int step = 0;
+            int total = 0;
+            do
+            {
+                (step, current[i]) = Stepper((step, current[i]));
+                total++;
+            } while (!current[i].EndsWith("Z"));
+            cycles[i] = total;
         }
 
-        long lcm = res.Select(t => (long)t.lam).LCM();
+        long lcm = cycles.Select(i => (long)i).LCM();
 
         return lcm;
     }
@@ -53,37 +55,6 @@ internal class Day08 : ASolution
         step = (step + 1) % _steps.Length;
 
         return (step, current);
-    }
-
-    // Floyd cycle detection
-    private (int, int) Floyd(Func<(int, string), (int, string)> f, (int, string) x0)
-    {
-        var tortoise = f(x0);
-        var hare = f(f(x0));
-        while (tortoise != hare)
-        {
-            tortoise = f(tortoise);
-            hare = f(f(hare));
-        }
-
-        var mu = 0;
-        tortoise = x0;
-        while (tortoise != hare)
-        {
-            tortoise = f(tortoise);
-            hare = f(hare);
-            mu += 1;
-        }
-
-        var lam = 1;
-        hare = f(tortoise);
-        while (tortoise != hare)
-        {
-            hare = f(hare);
-            lam += 1;
-        }
-
-        return (lam, mu);
     }
 
     private void ParseInput()
