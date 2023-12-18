365bfd30ea051953fdf89a7969a15fce096d0b13
Hank McCord <hymccord@gmail.com>
2023-12-18 16:00:38 -0500

    2023-13

---
+++ csharp/Solutions/Year2023/Day13.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode.Solutions.Year2023;
+
+internal class Day13 : ASolution
+{
+    public Day13() : base(00, 2022, "", false)
+    {
+
+    }
+
+    protected override object SolvePartOne()
+    {
+        return null;
+    }
+
+    protected override object SolvePartTwo()
+    {
+        return null;
+    }
+
+    protected override string LoadDebugInput()
+    {
+        return """
+        
+        """;
+    }
+}
