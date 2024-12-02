ac97cc58073efc80f3b3c6c0559e11d369aeebe9
Hank McCord <hymccord@gmail.com>
2024-12-02 00:15:47 -0500

    Add 2024 Day 01

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-Host.CreateDefaultBuilder(args)
+await Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config)=>
     {
         config.AddUserSecrets<Program>();
---
+++ csharp/Solutions/Year2024/Day01.cs
@@ -0,0 +1,54 @@
+﻿using MoreLinq;
+
+namespace AdventOfCode.Solutions.Year2024;
+
+internal class Day01 : ASolution
+{
+    private List<int> _left = [];
+    private List<int> _right = [];
+
+    public Day01() : base(1, 2024, "Historian Hysteria", false)
+    {
+
+    }
+
+    protected override object SolvePartOne()
+    {
+        foreach (var line in InputByNewLine)
+        {
+            if (line.ToIntArray() is [var l, var r])
+            {
+                _left.Add(l);
+                _right.Add(r);
+            }
+        }
+
+        _left.Sort();
+        _right.Sort();
+        
+        return _left.Zip(_right, (l, r) => Math.Abs(r - l)).Sum();
+    }
+
+    protected override object SolvePartTwo()
+    {
+        var counts = _right.CountBy(x => x).ToDictionary();
+
+        return _left.Sum(l =>
+        {
+            var r = counts.TryGetValue(l, out var c) ? c : 0;
+            return l * r;
+        });
+    }
+
+    protected override string LoadDebugInput()
+    {
+        return """
+        3   4
+        4   3
+        2   5
+        1   3
+        3   9
+        3   3
+        """;
+    }
+}
