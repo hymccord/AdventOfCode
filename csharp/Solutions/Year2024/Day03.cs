68cf90cc5e540008a3e772e8f444475077653832
Hank McCord <hymccord@gmail.com>
2024-12-03 15:14:10 -0500

    Add 2024 Day 03

---
+++ csharp/Solutions/Year2024/Day03.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Solutions.Year2024;
+
+internal class Day03 : ASolution
+{
+    public Day03() : base(03, 2024, "Mull It Over", false)
+    {
+
+    }
+
+    protected override object SolvePartOne()
+    {
+        return Scan(Input);
+    }
+
+    protected override object SolvePartTwo()
+    {
+        var undone = Regex.Replace(Input, @"don't\(\).*?(do\(\)|$)", "", RegexOptions.Singleline);
+        return Scan(undone);
+    }
+
+    private static long Scan(string input)
+    {
+        return Regex.Matches(input, @"mul\((\d{1,3}),(\d{1,3})\)", RegexOptions.Singleline)
+            .Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value));
+    }
+
+    protected override string LoadDebugInput()
+    {
+        return """
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+        """;
+    }
+}
